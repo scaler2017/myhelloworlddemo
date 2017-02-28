40e82ec879c2ba58fe668781faa2b639b350423e
scaler2017 <yangyu4608@gmail.com>
2017-02-28 21:56:29 +0800

    HelloWorld

---
+++ src/main/java/HelloWord.java
@@ -0,0 +1,10 @@
+/**
+ * Created by denghuang on 2017/2/28.
+ */
+public class HelloWord {
+    public static void main(String[] args){
+        String str = "Hello World";
+        System.out.print(str);
+
+    }
+}
